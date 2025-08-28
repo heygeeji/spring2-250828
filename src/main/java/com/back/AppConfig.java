0c0c95bd7b515e1bf4b9d265813279c63f1699d4
heygeeji <heygeeji@gmail.com>
2025-08-28 14:40:08 +0900

    @Component 방식으로 편하게 빈 등록

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,14 +1,13 @@
 package com.back;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //뭔가를 설정하는 클래스에 붙이는 어노테이션
 public class AppConfig {
 
-    @Bean
-    public PersonService personService() {
-        System.out.println("AppConfig.personService 호출됨!");
-        return new PersonService();
-    }
+//    @Bean //복잡한 객체 만들 때 사용
+//    public PersonService personService() {
+//        System.out.println("AppConfig.personService 호출됨!");
+//        return new PersonService();
+//    }
 }
