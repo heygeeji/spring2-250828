9f50fe683d35b79e331419504e8d12ba4bff60c7
heygeeji <heygeeji@gmail.com>
2025-08-29 10:29:03 +0900

    @Transactional, @Lazy 도입

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,25 +1,34 @@
 package com.back;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration //뭔가를 설정하는 클래스에 붙이는 어노테이션
 public class AppConfig {
+
+    @Autowired
+    @Lazy
+    private AppConfig self;
 
 
     @Bean
     public ApplicationRunner myApplicationRunner3() {
         return args -> {
-            work1();
-            work2();
+            self.work1();
+            self.work2();
         };
     }
 
+    @Transactional
     public void work1() {
         System.out.println("work1");
     }
 
+    @Transactional
     public void work2() {
         System.out.println("work2");
     }
