aeb68f0a13ea4b3407a69264da2d62a31cb00adb
heygeeji <heygeeji@gmail.com>
2025-08-29 09:54:20 +0900

    ApplicationRunner를 @Bean과 람다 조합으로 구현하도록 변경

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,8 +23,26 @@ public class AppConfig {
         return new PersonRepository(3);
     }
 
+    @Bean
+    public ApplicationRunner myApplicationRunner3() {
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    public void work1() {
+        System.out.println("work1");
+    }
+
+    public void work2() {
+        System.out.println("work2");
+    }
+
 //    public ApplicationRunner myApplicationRunner() { // 리턴타입이 이렇다는건 이런객체가 있으면 스프링부한테 빈으로 등록하라는 것
 //        return new ApplicationRunner();
 //    }
+
+
 
 }
