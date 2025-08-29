044fca92cd12578c418342e850c13514485c368a
heygeeji <heygeeji@gmail.com>
2025-08-29 11:31:09 +0900

    PersonService에 @Transactional 붙여서 프록시 확인

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,16 +1,16 @@
 package com.back;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@RequiredArgsConstructor
 public class PersonService {
 
     private final PersonRepository personRepository;
 
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
-
+    @Transactional
     public int count() {
         return personRepository.count();
     }
